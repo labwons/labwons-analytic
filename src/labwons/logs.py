3491133c4114ad6e03898ff87eebb8e88406c02a
labwons <>
2025-09-11 06:37:02 +0000

    COMMIT AND PUSH FROM COLAB

---
+++ src/labwons/logs.py
@@ -0,0 +1,42 @@
+from labwons.util import DATETIME, PATH
+import logging, os, time, sys
+
+
+def create_logger(file:str):
+    def kst(*args):
+        return time.localtime(time.mktime(time.gmtime(*args)) + 9 * 3600)
+
+    name = os.path.basename(file).split("_")[0]
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    logger.propagate = False
+
+    formatter = logging.Formatter(
+        fmt=f"%(asctime)s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+    formatter.converter = kst
+
+    file_handler = logging.FileHandler(file, encoding="utf-8")
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
+
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(logging.INFO)
+    console_handler.setFormatter(formatter)
+
+    if not logger.handlers:
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
+
+    return logger
+
+
+def read_log() -> str:
+    with open(os.path.join(PATH.LOGS, rf'{DATETIME.TODAY}.log'), 'r', encoding='utf-8') as f:
+        return f.read()
+
+
+os.makedirs(os.path.join(PATH.LOGS), exist_ok=True)
+logger = create_logger(os.path.join(PATH.LOGS, rf'{DATETIME.TODAY}.log'))
--- src/labwons/util.py
+++ src/labwons/util.py
@@ -4,8 +4,10 @@ from datetime import datetime, timedelta
 from pytz import timezone
 from pykrx.stock import get_nearest_business_day_in_a_week as get_td
 from typing import Dict, Union
+import os, re, requests
 
 
+PROJECT = 'labwons-analytic'
 TIMEZONE = timezone('Asia/Seoul')
 def CLOCK(strfmt: str = '') -> Union[str, datetime]:
     return datetime.now(TIMEZONE).strftime(strfmt) if strfmt else datetime.now(TIMEZONE)
@@ -23,6 +25,18 @@ class DATETIME:
                 return ''
         return getattr(cls, f'_TD_{cls.TODAY}')
 
+    @classproperty
+    def WISE(cls) -> str:
+        if not hasattr(cls, '__wi__'):
+            try:
+                date = re.compile(r"var\s+dt\s*=\s*'(\d{8})'") \
+                    .search(requests.get('https://www.wiseindex.com/Index/Index#/G1010.0.Components').text) \
+                    .group(1)
+                setattr(cls, '__wi__', date)
+            except (IndexError, Exception):
+                return None
+        return getattr(cls, '__wi__')
+
     @classmethod
     def get_previous_trading_dates(cls, *previous_days) -> Dict[str, str]:
         if not previous_days:
@@ -38,6 +52,22 @@ class DATETIME:
     def delta_today(cls, days:int):
         cls.TODAY = (CLOCK() - timedelta(days)).strftime('%Y%m%d')
         return
+
+
+class PATH:
+    if not os.environ.get("GITHUB_EVENT_NAME", ""):
+        find = os.path.dirname(__file__).split(os.sep).index(PROJECT)
+        ROOT = os.sep.join(os.path.dirname(__file__).split(os.sep)[:find + 1])
+    else:
+        ROOT = os.environ.get("GITHUB_WORKSPACE", os.getcwd())
+
+    SRC = os.path.join(ROOT, r'src')
+    DATA = os.path.join(ROOT, r'data')
+    
+    LOGS = os.path.join(DATA, r'logs')
+    TICKERS = os.path.join(DATA, r'tickers')
+    TIMESERIES = os.path.join(DATA, r'timeseries')
+
 
 
 if __name__ == "__main__":
